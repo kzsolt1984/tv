4d1c23ac6813e8bb00c943a9950bfbb0d9e0d9bd
kzs <kzsolt1984@gmail.com>
2017-01-16 11:41:05 +0100

    2 thumbnail, 3 page size

--- client/ts/main.ts
+++ client/ts/main.ts
@@ -4,15 +4,27 @@ module main {
     export class Main {
         constructor() {            
             this.recommender();
+            this.otherRecommender();
         }
         
         public recommender() {
-            var $box = $('.recommend-block').parent('.col-xs-4'),
-                $clone = $box.clone(true),
-                $cont = $('.live-recommender-container > .row');
-            
-            for (var i= 0; i< 3; i++) {
+            var $box = $('.recommend-block'),
                 
+                $cont = $('.live-recommender-container > div');
+            
+            for (var i= 0; i< 2; i++) {
+                var $clone = $box.clone();
+                 $cont.append($clone);
+            }
+        }
+        
+        public otherRecommender() {
+            var $box = $('.other-recommender-container .thumbnail-big'),
+                
+                $cont = $('.other-recommender-container > div');
+            
+            for (var i= 0; i< 7; i++) {
+                var $clone = $box.clone();
                  $cont.append($clone);
             }
         }
